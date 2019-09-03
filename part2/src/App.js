c0e112f0cc53a3b0d62f1969ca4e3547239515bb
Steven <stevenle18@gmail.com>
2019-09-02 20:48:18 -0700

    part2 2.7 Complete

---
+++ part2/src/App.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+
+const isDuplicate = (persons, newName) => {
+  const map = new Map();
+  for(var i = 0; i < persons.length; i++){ 
+    map.set(persons[i].name, 1)
+  }
+  if(map.has(newName)){
+    return true
+  } else {
+    return false
+  }
+}
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const addName = (event) => {
+      event.preventDefault()
+      const personAdd = {
+        name: newName
+      }      
+
+      if(isDuplicate(persons, newName)){
+        alert(`${newName} is already added to phonebook`)
+      } else {
+        setPersons(persons.concat(personAdd))
+        setNewName('')
+      }
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      {/* {console.log(persons)} */}
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+    </div>
+  )
+}
+
+export default App
