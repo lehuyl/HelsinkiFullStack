6bf8eb8a0d605027d62e5c58403c2e03be8a9f5c
Steven <stevenle18@gmail.com>
2019-09-10 23:32:35 -0700

    Complete 2.14 Weather and added StackOverflow reference

---
+++ part2notes/src/App.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from 'react'
+import Note from './components/Note'
+import axios from 'axios'
+
+const App = (props) => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('a new note...')
+  const [showAll, setShowAll] = useState(true)
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }
+  //effects only run after completed render
+  //empty array means effect only run along first render
+  useEffect(hook, [])
+
+  console.log('render', notes.length, 'notes')
+
+  const notesToShow = showAll
+  //if showAll is true then notes else notes.filter
+    ? notes
+    : notes.filter(note => note.important)
+
+  const rows = () => notesToShow.map(note =>
+    <Note
+      key={note.id}
+      note={note}
+    />
+  )
+
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString,
+      important: Math.random() > 0.5,
+      id: notes.length + 1,
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+  
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+      <ul>
+        {rows()}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type="submit">save</button>
+      </form> 
+    </div>
+  )
+}
+
+export default App
