934d97d4178491d217d21687b46f0e7d97e5b28c
Steven <stevenle18@gmail.com>
2019-09-15 00:42:27 -0700

    Testing MongoDB and mongoose

---
+++ part3/phonebookbackend/mongo.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose')
+
+if( process.argv.length < 3){
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+    `mongodb+srv://fullstack:${password}@phonebookdata-2zyut.mongodb.net/test?retryWrites=true&w=majority`
+
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+    id: Number
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+    id: Math.floor(Math.random() * 100)
+})
+
+if(process.argv.length == 5){
+    person.save().then(response => {
+        console.log('person saved')
+        mongoose.connection.close()
+    })
+}
+
+if(process.argv.length == 3){
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(person)
+        })
+        mongoose.connection.close()
+    })
+}
+
+
+
+// if(process.argv.length == 3){
+//     const password = process.argv[2]
+
+//     const url =
+//         `mongodb+srv://fullstack:${password}@phonebookdata-2zyut.mongodb.net/test?retryWrites=true&w=majority`
+    
+//     mongoose.connect(url, {useNewUrlParser: true})
+
+//     Person.find({}).then(result => {
+//         result.forEach(person => {
+//             console.log(person)
+//         })
+
+//         mongoose.connection.close()
+//     })
+// }
