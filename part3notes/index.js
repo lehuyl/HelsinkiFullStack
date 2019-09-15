d3c17147b8a3cdeb4421a48fa723ef1d53d02526
Steven <stevenle18@gmail.com>
2019-09-15 00:13:39 -0700

    Initial testing of MongoDB

--- part3notes/index.js
+++ part3notes/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const mongoose = require('mongoose')
 
 app.use(bodyParser.json())
 app.use(cors())
